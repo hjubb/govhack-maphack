0f4a301ccc5ddf00f4eba5ffb153183bf23c1b5c
Lanzafame <adrianlanzafame92@gmail.com>
2015-07-04 23:05:53 +1000

    # This is a combination of 2 commits. # The first commit's message is:
    Updated parser. It now adds lighting and surface columns to the weightedlocations.csv. It is also a lot more elegant and makes adding any other columns really easy :D
    
    # This is the 2nd commit message:
    
    updated gitignore

--- app/data/parser.jl
+++ app/data/parser.jl
@@ -1,38 +1,46 @@
 #= Severity,Lon,Lat,Fatality,Hospitalised,MedicallyTreated,MinorInjury =#
 
-data = readcsv("locations.csv")
+data = readcsv("locations-all.csv")
 
-severity = data[2:size(data,1)]
-uniqueSeverity = unique(severity)
+severity = data[2:size(data,1),2]
+lon = data[2:size(data,1),9]
+lat = data[2:size(data,1),10]
+surface = data[2:size(data,1),31]
+lighting = data[2:size(data,1),33]
+fatal = data[2:size(data,1),39]
+hospital = data[2:size(data,1),40]
+medtreat = data[2:size(data,1),41]
+injury = data[2:size(data,1),42]
 
-function severity2Int(s)
-  if s == "Fatal"
-    return 5
-  elseif s == "Hospitalisation"
-    return 4
-  elseif s == "Medical treatment"
-    return 3
-  elseif s == "Minor injury"
-    return 2
-  else
-    return 1
-  end
+sevdict = {"Fatal"=>5,"Hospitalisation"=>4,"Medical treatment"=>3,"Minor injury"=>2,"Property damage only"=>1}
+
+lightdict = {"Darkness - Not lighted"=>1, "Daylight"=>2, "Darkness - Lighted"=>3, "Dawn/Dusk"=>4, "Unknown"=>5}
+
+surfacedict = {"Unsealed - Dry"=>1,"Sealed - Dry"=>2,"Sealed - Wet"=>3,"Unknown"=>4,"Unsealed - Wet"=>5}
+
+
+function lightToInt(v)
+  get(lightdict, v, 0)
 end
 
-sevint = map(severity2Int, severity)
-lon = data[2:size(data,1),2]
-lat = data[2:size(data,1),3]
-fatal = data[2:size(data,1),4]
-hospital = data[2:size(data,1),5]
-medtreat = data[2:size(data,1),6]
-injury = data[2:size(data,1),7]
+function surfaceToInt(v)
+  get(surfacedict, v, 0)
+end
+
+function severityToInt(v)
+  get(sevdict, v, 0)
+end
 
 function cumSeverity(s, f, h, m, i)
   return +(s, f, h, m, i)
 end
 
+sevint = map(severityToInt, severity)
+surfaceint = map(surfaceToInt, surface)
+lightint = map(lightToInt, lighting)
+
 totalSeverity = map(cumSeverity, sevint, fatal, hospital, medtreat, injury)
 
-finalCsv = hcat(lon, lat, totalSeverity)
+finalCsv = hcat(lon, lat, totalSeverity, surfaceint, lightint)
 
 writecsv("weightedlocations.csv", finalCsv)
